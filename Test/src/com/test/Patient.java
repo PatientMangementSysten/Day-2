605e66f1e6e63bdcc535f9f3b631a178dc455be9
Pratik Tiwari <PratikT2@citiustech.com>
2021-03-22 21:03:44 +0530

    commit

---
+++ Test/src/com/test/Patient.java
@@ -0,0 +1,55 @@
+package com.test;
+
+public class Patient {
+
+	private String Name;
+	
+	private int Id;
+	
+	private String Status;
+	
+	private int contactNumber;
+	
+	private String allgeris;
+
+	public String getAllgeris() {
+		return allgeris;
+	}
+
+	public void setAllgeris(String allgeris) {
+		this.allgeris = allgeris;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getStatus() {
+		return Status;
+	}
+
+	public void setStatus(String status) {
+		Status = status;
+	}
+
+	public int getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(int contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+}
---
+++ Test/src/com/test/Test.java
@@ -0,0 +1,17 @@
+package com.test;
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test.main()");
+	
+		String name  = System.getProperty("user.name");
+		
+		System.out.println(name);
+	}
+	
+	 
+}
